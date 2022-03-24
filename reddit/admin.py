eee18281bddfa904b1dcd0daf3feaa99b7b21285
Sarah Jameson <meowells@gmail.com>
2022-03-24 09:45:05 +0000

    Finished admin panel

--- reddit/admin.py
+++ reddit/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ('title', 'content')
 
+@admin.register(Comment)
+class CommmentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('created_on', 'name')
+    search_fields = ('name', 'email', 'body')
