e8f033f5d81c3be199f5d1cd0636be77ed7d8844
Sarah Jameson <meowells@gmail.com>
2022-04-10 23:49:46 +0000

    Changed urls file to adhere to pep8 conventions

--- reddit/urls.py
+++ reddit/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('create-post', views.PostCreate.as_view(), name='create_post'),
     path('edit/<slug:slug>', views.UpdatePost.as_view(), name='update_post'),
-    path('delete-post/<slug:slug>', views.DeletePost.as_view(), name='delete_post'),
+    path(
+        'delete-post/<slug:slug>',
+        views.DeletePost.as_view(),
+        name='delete_post'
+    ),
     path('user-profile', views.ProfilePostList.as_view(), name='user_profile'),
     path('search-posts', views.SearchPosts.as_view(), name='search_posts'),
     ]
